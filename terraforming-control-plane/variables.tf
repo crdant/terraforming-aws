fbb904f24306accd474f193ff72b5f12b72ad0be
Chuck D'Antonio <c_dantonio@acm.org>
2018-11-20 13:47:11 -0500

    Pare down to variables we will use.

--- terraforming-control-plane/variables.tf
+++ terraforming-control-plane/variables.tf
@@ -21,18 +21,6 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-/******
-* PAS *
-*******/
-
-variable "create_versioned_pas_buckets" {
-  default = false
-}
-
-variable "create_backup_pas_buckets" {
-  default = false
-}
-
 /****************
 * Ops Manager *
 *****************/
@@ -79,68 +67,6 @@ variable "rds_instance_count" {
   default = 0
 }
 
-/*******************
-* SSL Certificates *
-********************/
-
-variable "ssl_cert_arn" {
-  type        = "string"
-  description = "The ARN for the certificate to be used by the LB, optional if `ssl_cert` or `ssl_ca_cert` is provided"
-  default     = ""
-}
-
-variable "ssl_cert" {
-  type        = "string"
-  description = "the contents of an SSL certificate to be used by the LB, optional if `ssl_cert_arn` or `ssl_ca_cert` is provided"
-  default     = ""
-}
-
-variable "ssl_private_key" {
-  type        = "string"
-  description = "the contents of an SSL private key to be used by the LB, optional if `ssl_cert_arn` or `ssl_ca_cert` is provided"
-  default     = ""
-}
-
-variable "ssl_ca_cert" {
-  type        = "string"
-  description = "the contents of a CA public key to be used to sign the generated LB certificate, optional if `ssl_cert_arn` or `ssl_cert` is provided"
-  default     = ""
-}
-
-variable "ssl_ca_private_key" {
-  type        = "string"
-  description = "the contents of a CA private key to be used to sign the generated LB certificate, optional if `ssl_cert_arn` or `ssl_cert` is provided"
-  default     = ""
-}
-
-/*****************************
- * Isolation Segment Options *
- *****************************/
-
-variable "isoseg_ssl_cert" {
-  type        = "string"
-  description = "the contents of an SSL certificate to be used by the LB, optional if `isoseg_ssl_ca_cert` is provided"
-  default     = ""
-}
-
-variable "isoseg_ssl_private_key" {
-  type        = "string"
-  description = "the contents of an SSL private key to be used by the LB, optional if `isoseg_ssl_ca_cert` is provided"
-  default     = ""
-}
-
-variable "isoseg_ssl_ca_cert" {
-  type        = "string"
-  description = "the contents of a CA public key to be used to sign the generated iso seg LB certificate, optional if `isoseg_ssl_cert` is provided"
-  default     = ""
-}
-
-variable "isoseg_ssl_ca_private_key" {
-  type        = "string"
-  description = "the contents of a CA private key to be used to sign the generated iso seg LB certificate, optional if `isoseg_ssl_cert` is provided"
-  default     = ""
-}
-
 /********
 * Tags *
 *********/
@@ -149,14 +75,4 @@ variable "tags" {
   type        = "map"
   default     = {}
   description = "Key/value tags to assign to all AWS resources"
-}
-
-/**************
-* Deprecated *
-***************/
-
-variable "create_isoseg_resources" {
-  type        = "string"
-  default     = "0"
-  description = "Optionally create a LB and DNS entries for a single isolation segment. Valid values are 0 or 1."
 }
